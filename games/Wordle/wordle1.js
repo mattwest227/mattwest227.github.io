cdf7c1d137d84ab1d29148cd4646f4edb9d18b4b
mattwest227 <101122075+mattwest227@users.noreply.github.com>
2023-03-15 11:55:20 -0400

    Add files via upload

---
+++ games/Wordle/wordle1.js
@@ -0,0 +1,100 @@
+let word = ["cigar", "rebut", "blush", "focal", "heath", "dwarf", "model", "stink", "grade", "quiet", "bench", "feign", "major", "death", "fresh", "crust", "react", "pride", "helix", "croak", "unfed", "whelp", "trawl", "adobe", "crazy", "sower", "repay", "crate", "spike", "pound", "unmet", "flesh", "forth", "first", "stand", "ivory", "print", "yearn", "drain", "panel", "flume", "swirl", "argue", "delta", "flick", "front", "shrub", "biome", "goner", "golem", "lusty", "round", "audit", "lying", "labor", "islet", "forge", "corny", "moult", "basic", "spicy", "spray", "fjord", "spend", "guild", "alone", "hyper", "thumb", "dowry", "ought", "belch", "dutch", "pilot", "comet", "jaunt", "growl", "fling", "dozen", "world", "great", "blurt", "coast", "duchy", "groin", "fixer", "group", "rogue", "badly", "smart", "pithy", "gaudy", "heron", "vodka", "finer", "radio", "rouge", "perch", "retch", "wrote", "tilde", "store", "prove", "bring", "solve", "cheat", "grime", "exult", "usher", "epoch", "triad", "break", "rhino", "viral", "sonic", "vital", "trace", "using", "peach", "champ", "baton", "brake", "pluck", "craze", "gripe", "weary", "picky", "acute", "aside", "tapir", "unify", "rebus", "tiger", "slump", "crank", "query", "drink", "favor", "tangy", "panic", "solar", "shire", "proxy", "point", "prick", "wince", "crimp", "sugar", "whack", "mount", "perky", "could", "wrung", "light", "those", "moist", "shard", "pleat", "aloft", "frame", "humor", "pause", "ulcer", "ultra", "robin", "caulk", "shake", "other", "thorn", "trove", "bloke", "chant", "choke", "nasty", "mourn", "brine", "cloth", "hoard", "month", "lapse", "watch", "today", "focus", "smelt", "tease", "cater", "movie", "lynch", "saute", "their", "purge", "chest", "depot", "epoxy", "nymph", "found", "stove", "snout", "trope", "shawl", "fibre", "foray", "scare", "stair", "black", "squad", "royal", "chunk", "mince", "slave", "shame", "ample", "flair", "foyer", "cargo", "oxide", "plant", "olive", "inert", "askew", "heist", "shown", "zesty", "hasty", "trash", "story", "hairy", "train", "homer", "badge", "midst", "fetus", "butch", "farce", "slung", "tipsy", "metal", "yield", "being", "scour", "gamer", "scrap", "money", "hinge", "album", "vouch", "crept", "bayou", "manor", "creak", "showy", "phase", "froth", "depth", "girth", "piety", "payer", "float", "atone", "primo", "apron", "blown", "loser", "input", "gloat", "awful", "brink", "smite", "beady", "rusty", "gawky", "pinto", "gaily", "field", "hydro", "flack", "wench", "voice", "stead", "stalk", "berth", "night", "bland", "liver", "wacky", "flock", "angry", "aphid", "midge", "power", "stomp", "upset", "cramp", "quart", "youth", "rhyme", "alien", "smear", "unfit", "cling", "glean", "hunky", "poker", "gruel", "twice", "twang", "shrug", "unlit", "waste", "merit", "woven", "octal", "clown", "irony", "ruder", "gauze", "chief", "onset", "prize", "fungi", "charm", "inter", "lofty", "thyme", "eclat", "doubt", "chute", "stick", "trice", "alike", "recap", "saint", "glory", "grate", "admit", "brisk", "scald", "scorn", "twine", "sting", "bough", "marsh", "sloth", "vigor", "howdy", "enjoy", "valid", "equal", "unset", "spade", "stein", "exist", "quirk", "denim", "grove", "spiel", "fault", "flout", "sneak", "waltz", "aptly", "piney", "inept", "aloud", "dream", "stale", "vomit", "ombre", "unite", "snarl", "baker", "glyph", "louse", "gulch", "vault", "godly", "threw", "grave", "shock", "crave", "spite", "skimp", "claim", "rainy", "musty", "pique", "quasi", "arise", "valet", "opium", "avert", "stuck", "recut", "mulch", "plume", "rifle", "count", "incur", "wrest", "mocha", "study", "lover", "safer", "rivet", "smoke", "mound", "undue", "sedan", "swine", "guile", "gusty", "equip", "tough", "canoe", "chaos", "covet", "human", "lunch", "blast", "stray", "lefty", "quick", "paste", "given", "risen", "groan", "leaky", "grind", "carve", "sadly", "spilt", "slack", "honey", "final", "minty", "slick", "derby", "wharf", "spelt", "erupt", "singe", "price", "spawn", "fairy", "filmy", "stack", "chose", "handy", "grace", "stank", "cream", "valor", "angle", "chase", "reply", "prone", "spoil", "heart", "shade", "diner", "arson", "dowel", "palsy", "bowel", "smile", "lance", "siren", "built", "frown", "patio", "laden", "humid", "lymph", "edify", "might", "gusto", "purse", "vapor", "write", "loath", "slide", "venom", "stamp", "acorn", "aping", "pushy", "tamer", "hater", "awoke", "brawn", "swift", "birch", "lucky", "risky", "ghost", "plier", "lunar", "winch", "snare", "nurse", "house", "borax", "nicer", "lurch", "exalt", "about", "toxin", "tunic", "pried", "inlay", "chump", "lanky", "boule", "place", "plush", "index", "blink", "clung", "qualm", "croup", "clink", "juicy", "stage", "decay", "flier", "shaft", "clean", "china", "ridge", "vowel", "gnome", "snuck", "spiny", "snail", "flown", "rabid", "prose", "thank", "budge", "fiber", "moldy", "track", "dumpy", "paler", "swore", "rebar", "scuba", "splat", "flyer", "horny", "mason", "doing", "amply", "molar", "ovary", "magic", "truce", "sport", "fritz", "edict", "twirl", "range", "whisk", "hovel", "rehab", "sigma", "spout", "dying", "fetid", "brain", "thump", "scion", "candy", "chord", "basin", "march", "crowd", "musky", "stain", "bravo", "stung", "ruler", "blond", "layer", "fluid", "score", "cutie", "zebra", "barge", "matey", "bluer", "aider", "privy", "frisk", "begun", "azure", "sound", "glove", "braid", "scope", "ocean", "irate", "later", "woken", "silky", "wreck", "dwelt", "slate", "smack", "solid", "hazel", "wrist", "globe", "flint", "rouse", "vista", "relax", "cover", "alive", "vocal", "often", "eight", "joker", "since", "shunt", "diver", "poser", "worst", "anime", "leafy", "dunce", "stare", "pudgy", "waive", "choir", "spoke", "delay", "bilge", "ideal", "clasp", "hotly", "laugh", "block", "meant", "grape", "hardy", "shied", "drawl", "daisy", "burnt", "tulip", "crick", "vixen", "cough", "naive", "shoal", "stork", "bathe", "aunty", "prime", "outer", "evict", "imply", "demur", "quota", "haven", "cavil", "swear", "crump", "dough", "gavel", "wagon", "salon", "nudge", "harem", "pitch", "sworn", "stony", "cabin", "unzip", "earth", "storm", "until", "taper", "child", "adopt", "minor", "husky", "brave", "filet", "slime", "glint", "tread", "steal", "regal", "guest", "murky", "share", "spore", "hoist", "buxom", "dimly", "sumac", "donut", "scrub", "fancy", "slimy", "pearl", "porch", "dingo", "sepia", "amble", "shady", "bread", "reign", "dairy", "tuber", "shear", "posit", "blank", "shank", "freak", "among", "fecal", "would", "large", "agony", "amuse", "bushy", "copse", "knife", "pouch", "ascot", "plane", "crown", "urban", "snide", "relay", "abide", "viola", "straw", "crash", "third", "trick", "grief", "disco", "beach", "clued", "caste", "graze", "frock", "gonad", "drunk", "prong", "lurid", "halve", "buyer", "vinyl", "utile", "trade", "finch", "ashen", "modal", "gaunt", "clove", "enact", "adorn", "roast", "speck", "sheik", "grunt", "party", "touch", "south", "vapid", "angst", "tubal", "lower", "crest", "sweat", "cyber", "adore", "tardy", "swami", "notch", "roach", "young", "align", "ready", "frond", "strap", "realm", "swarm", "diary", "drank", "acrid", "heady", "junto", "quoth", "bonus", "shalt", "amend", "datum", "build", "piano", "shelf", "lodge", "suing", "rearm", "coral", "ramen", "worth", "psalm", "infer", "overt", "mayor", "glide", "usage", "poise", "randy", "prank", "fishy", "drove", "idler", "swath", "while", "begat", "slang", "credo", "shift", "timer", "bylaw", "serum", "steak", "shirk", "blunt", "penal", "joist", "shape", "adept", "stole", "topaz", "chore", "fluke", "bloat", "caper", "boxer", "jumbo", "lunge", "space", "short", "slurp", "loyal", "flirt", "tempo", "plate", "plunk", "afoul", "savoy", "agile", "stake", "knave", "beard", "arose", "motif", "broil", "glare", "shove", "swamp", "along", "rugby", "wager", "quack", "squat", "snaky", "debit", "mange", "skate", "joust", "tramp", "spurn", "medal", "micro", "flank", "learn", "nadir", "maple", "comfy", "remit", "least", "mogul", "fetch", "cause", "oaken", "aglow", "meaty", "prowl", "thief", "stern", "poesy", "rocky", "waist", "spire", "grope", "havoc", "patsy", "truly", "forty", "deity", "uncle", "giver", "lemur", "draft", "slope", "lingo", "bleak", "curly", "cedar", "dirge", "grown", "horde", "shuck", "crypt", "cumin", "stock", "gravy", "locus", "wider", "quite", "chafe", "blimp", "deign", "fiend", "logic", "cheap", "false", "renal", "rowdy", "blaze", "envoy", "brief", "abort", "mouse", "mucky", "sulky", "fiery", "media", "trunk", "yeast", "clear", "scalp", "cider", "duvet", "super", "after", "owner", "reach", "nobly", "empty", "gipsy", "smock", "burst", "amity", "shaky", "hover", "carol", "snort", "synod", "faint", "haunt", "flour", "chair", "detox", "shrew", "plied", "quark", "burly", "novel", "waxen", "stoic", "jerky", "blitz", "lyric", "towel", "quilt", "below", "bingo", "wispy", "brash", "scone", "saucy", "value", "spice", "route", "sharp", "bawdy", "phony", "lager", "urine", "trial", "flora", "latch", "wight", "brick", "decal", "shack", "dogma", "mover", "sober", "optic", "vying", "nomad", "flute", "shark", "bugle", "tawny", "chalk", "feast", "pedal", "scarf", "cruel", "bleat", "tidal", "windy", "dusty", "nerdy", "shone", "whale", "hymen", "abuse", "elbow", "crumb", "pansy", "welsh", "syrup", "suave", "gamut", "swung", "drake", "afire", "shirt", "grout", "plaid", "blind", "torch", "tying", "pesky", "alter", "gazer", "noble", "ethos", "bride", "extol", "decor", "beast", "sixth", "spunk", "scaly", "scold", "hefty", "whiny", "quake", "joint", "swept", "prude", "heavy", "wield", "maize", "shale", "screw", "smoky", "scent", "glade", "spent", "prism", "stoke", "riper", "orbit", "guilt", "table", "smirk", "wrong", "noisy", "alert", "shiny", "resin", "whole", "pixel", "polar", "hotel", "sword", "cleat", "mango", "rumba", "leash", "clout", "dance", "ovate", "facet", "paint", "liner", "curio", "salty", "audio", "snake", "fable", "cloak", "navel", "spurt", "pesto", "balmy", "flash", "unwed", "early", "churn", "stump", "wimpy", "saner", "blend", "thick", "warty", "manic", "blare", "squib", "probe", "force", "debut", "haste", "agent", "widen", "slice", "ingot", "clash", "abode", "throw", "unity", "pivot", "slept", "spare", "parse", "morph", "tacky", "demon", "begin", "patch", "water", "lumpy", "admin", "omega", "macho", "aisle", "plank", "botch", "crawl", "lousy", "slain", "raise", "wrack", "guide", "foist", "cameo", "under", "actor", "revue", "fraud", "harpy", "climb", "olden", "clerk", "debar", "ethic", "cairn", "ghoul", "crude", "scale", "older", "plain", "sperm", "briny", "quest", "crisp", "bound", "befit", "drawn", "suite", "itchy", "bagel", "broad", "axiom", "chard", "caput", "leant", "curse", "proud", "swing", "opine", "gumbo", "miner", "chasm", "topic", "brush", "crane", "mural", "abled", "habit", "maker", "dusky", "lithe", "giant", "surly", "flunk", "began", "prune", "slant", "torus", "covey", "viper", "taken", "moral", "vogue", "owing", "token", "entry", "voter", "chide", "elfin", "ebony", "neigh", "melon", "decoy", "voila", "ankle", "mushy", "tribe", "birth", "graph", "weird", "tried", "rough", "weigh", "strip", "craft", "minus", "dicey", "lucid", "vicar", "ditch", "flame", "sight", "broke", "teary", "chart", "sixty", "wordy", "nosey", "bulge", "savor", "clamp", "funky", "foamy", "toxic", "brand", "plumb", "dingy", "tripe", "bicep", "tenor", "worse", "hyena", "think", "ratio", "cobra", "basil", "scrum", "bused", "phone", "court", "camel", "heard", "angel", "petal", "pouty", "throb", "maybe", "fetal", "sprig", "spine", "shout", "cadet", "macro", "satyr", "binge", "trend", "leapt", "split", "width", "sonar", "tower", "baron", "waver", "spark", "smote", "baler", "above", "north", "wafer", "scant", "snack", "scowl", "frail", "drift", "limbo", "motel", "ounce", "wreak", "talon", "knelt", "flake", "debug", "anode", "crime", "salve", "scout", "imbue", "pinky", "stave", "vague", "fight", "video", "stone", "teach", "cleft", "frost", "prawn", "tweak", "board", "grant", "medic", "bacon", "cable", "brawl", "slunk", "raspy", "forum", "drone", "women", "boast", "coven", "tumor", "wrath", "steam", "daily", "trail", "niche", "mealy", "juice", "nylon", "wheat", "cower", "brute", "snipe", "sinew", "skier", "jumpy", "umbra", "scary", "satin", "tonic", "stark", "trump", "avoid", "quote", "devil", "liken", "gayer", "noise", "gland", "dealt", "sling", "opera", "tonga", "flare", "wound", "white", "bulky", "horse", "inbox", "grain", "surge", "gleam", "salvo", "crush", "fruit", "taker", "ovine", "spiky", "frank", "filth", "right", "clank", "lumen", "borne", "sauce", "amber", "lathe", "dirty", "mauve", "flung", "yacht", "fried", "quail", "brunt", "grimy", "curvy", "cagey", "rinse", "grasp", "milky", "bison", "graft", "sandy", "baste", "flask", "girly", "boney", "coupe", "endow", "abhor", "welch", "blade", "miser", "mirth", "cloud", "close", "pecan", "droit", "grail", "clone", "guise", "ralph", "tango", "smith", "mower", "serif", "drape", "spank", "glaze", "truck", "virus", "zonal", "metro", "grand", "banjo", "axion", "chain", "filer", "brace", "pubic", "raven", "plead", "gnash", "flaky", "munch", "eking", "thing", "slink", "shorn", "ranch", "wring", "lemon", "shore", "froze", "style", "antic", "drown", "vegan", "image", "exact", "dopey", "spear", "cried", "chime", "crony", "stunk", "batch", "curve", "witch", "bunch", "anvil", "soapy", "broth", "madly", "woman", "thong", "punch", "pasty", "downy", "knead", "whirl", "rapid", "clang", "anger", "drive", "email", "music", "setup", "verso", "quash", "newly", "relic", "fudge", "femur", "chirp", "forte", "whine", "plait", "fleck", "felon", "gourd", "brown", "thrum", "ficus", "decry", "wiser", "junta", "visor", "daunt", "impel", "whose", "turbo", "speak", "mangy", "eying", "inlet", "crone", "pulse", "staid", "pinch", "snore", "ripen", "snowy", "leach", "mouth", "hound", "clump", "tonal", "bigot", "peril", "blame", "haute", "spied", "intro", "shine", "gecko", "guard", "loamy", "scamp", "scram", "manly", "vaunt", "organ", "feral", "extra", "polka", "rayon", "skirt", "faith", "match", "mercy", "tepid", "flush", "login", "rival", "untie", "refit", "adult", "judge", "artsy", "shave"];
+        
+// auto tab
+let field = 0;
+function autoTab(field) {
+  document.getElementById(field).focus()
+}
+
+let wrongLetters = [];
+
+//function randomWord() {
+//chooses a random word from the word array
+
+let randNum = Math.floor(Math.random() * word.length);
+let randWord = word[randNum];
+//alert(randWord)
+
+//changes the word to an array
+let wordArr = Array.from(String(randWord));
+//document.write(wordArr)
+
+//listens for click on enter to create user word for that try
+document.getElementById("btn1").addEventListener('click', () => {
+  createWord()}); 
+
+document.getElementById("btn2").addEventListener('click', () => {
+    createWord()}); 
+  
+document.getElementById("btn3").addEventListener('click', () => {
+      createWord()}); 
+    
+document.getElementById("btn4").addEventListener('click', () => {
+        createWord()}); 
+      
+document.getElementById("btn5").addEventListener('click', () => {
+          createWord()}); 
+        
+document.getElementById("btn6").addEventListener('click', () => {
+            createWord()}); 
+  
+            
+// reads the array of letters in the users word
+//multiplies by 5 to move to the proper numbered word guess   
+let x = 0;
+
+
+  function createWord() {
+  let a = document.getElementById(`${5*x+0}`).value;
+  let b = document.getElementById(`${5*x+1}`).value;
+  let c = document.getElementById(`${5*x+2}`).value;
+  let d = document.getElementById(`${5*x+3}`).value;
+  let e = document.getElementById(`${5*x+4}`).value;
+  let chosenWord = a+b+c+d+e;
+  let currLetter = Array.from(String(chosenWord));
+
+
+  // loop through users guess
+// p counts to see if there was a hit on any letter or print a _
+for (let i = 0; i < 5; i++) {
+  let p = 0;
+
+//loop through computer word
+for (let j = 0; j < 5; j++) {
+
+  //checks if user letter at space [i] matches computer letter at space [j]
+
+  //adds class "right-spot" if letter is correct and in the right space
+   if (currLetter[i] === wordArr[j] && i === j) {
+    document.getElementById(`${5*x+i}`).classList.add("right-spot");
+      p++
+      }
+
+  //adds class "wrong-spot" if letter matches but is in the wrong space    
+     else if (currLetter[i] === wordArr[j] && i !== j) {
+        document.getElementById(`${5*x+i}`).classList.add("wrong-spot");
+      p++
+      } 
+
+  //adds class "wrong-letter" if there is no match 
+    else if (j === 4 && p === 0) {
+        wrongLetters.push(currLetter[i]);
+        document.getElementById(`${5*x+i}`).classList.add("wrong-letter");
+      }
+  }
+}
+document.getElementById('wrong').innerText = wrongLetters ;
+
+if (randWord === chosenWord) {
+  document.getElementById('wrong').innerText = `CONGRATULATIONS! 
+  You got the word ${randWord} in ${x+1} tries.` ;
+}
+
+if (x >= 5 && randWord != chosenWord)  {
+document.getElementById('wrong').innerText = `Sorry. You didn't get the word dummy! 
+It was ${randWord}`}  
+
+  x++;
+  autoTab(`${x*5}`);
+
+} 
