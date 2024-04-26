48a8b6ced5fdbf3dbba0cb972cc18717b567442f
mattwest227 <101122075+mattwest227@users.noreply.github.com>
2024-04-26 16:22:44 -0400

    Update VBall_phaser copy.js

--- games/VBall_Phaser 2/VBall_phaser copy.js
+++ games/VBall_Phaser 2/VBall_phaser copy.js
@@ -22,9 +22,9 @@ const game = new Phaser.Game(config);
 const gameState = {};
 
 function preload() {
-    this.load.image('ball', '/ball.png');
-    this.load.image('player', '/star_88.png');
-    this.load.image('background', '/VBackground.png');
+    this.load.image('ball', '/games/VBall_Phaser 2/ball.png');
+    this.load.image('player', '/games/VBall_Phaser 2/star_88.png');
+    this.load.image('background', '/games/VBall_Phaser 2/VBackground.png');
 };
 
 
